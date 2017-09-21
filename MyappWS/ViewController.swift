59051445574678454501ed49de75b1ecd9e619a6
Irina <IrinaQA@MacBook-Pro.local>
2017-09-21 13:36:58 +0200

    Initial Commit

---
+++ MyappWS/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MyappWS
+//
+//  Created by Irina on 21/09/2017.
+//  Copyright © 2017 Irina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print ("Ahoj")
+    
+    mojeFunkce()
+    }
+
+    
+  @IBAction  func mojeFunkce( ){
+        
+        print("Jak se mas?")
+        
+    }
+    
+    
+}
+
